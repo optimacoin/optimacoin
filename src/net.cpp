ed72e8b33c16712de0c94a42d8c8de08dd0142cd
optimacoin <optimacoin2018@gmail.com>
2018-02-04 22:00:01 +0100

    sekond commit

--- src/net.cpp
+++ src/net.cpp
@@ -1191,10 +1191,17 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+<<<<<<< HEAD
     {"node.optimacoin.website", "seed.optimacoin.website"},
     {"abacus.optimacoin.website", "fledge.optimacoin.website"},
     {"mainnet.optimacoin.pw", "seed.mainnet.optimacoin.pw"},
     {"seed.optimacoin.website", "dnsseed.optimacoin.website"},
+=======
+    {"node.freico.in", "seed.freico.in"},
+    {"abacus.freico.in", "fledge.freico.in"},
+    {"mainnet.optimacoin.pw", "seed.mainnet.optimacoin.pw"},
+    {"seed.sicanet.net", "dnsseed.sicanet.net"},
+>>>>>>> d8f5cdc... gbsogbndgbnsgbngbns[bgion
     {NULL, NULL}
 };
 
